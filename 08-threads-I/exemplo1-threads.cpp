8a3ae828b6e27722ed4aa570d2bca2d38a6c85ca
eduardotp1 <edu.tirta@gmail.com>
2019-09-03 23:34:04 -0300

    Aula 1 threads done

--- 08-threads-I/exemplo1-threads.cpp
+++ 08-threads-I/exemplo1-threads.cpp
@@ -1,27 +1,48 @@
 #include <thread>
 #include <iostream>
+#include <vector>
+
 
 void funcao_rodando_em_paralelo(int a, int *b) {
     std::cout << "a=" << a << std::endl;
-    *b = 5;
+    *b=a*a;
 }
 
 
 int main() {
-    int b = 10;
+    int n_threads = std::thread::hardware_concurrency();
+    std::vector<std::thread> v(n_threads);
+    std::vector<int> b(n_threads);
+    std::cout << "Number of Threads: " << n_threads << std::endl;
 
     // Cria thread e a executa.
     // Primeiro argumento é a função a ser executada.
     
     // Os argumentos em seguida são passados diretamente
     // para a função passada no primeiro argumento.
-    std::thread t1(funcao_rodando_em_paralelo, 15, &b);
+
+    for(int i =0; i<n_threads;i++){
+        v[i]=std::thread(funcao_rodando_em_paralelo, i, &b[i]);
+    }
+    for(int i =0; i<n_threads;i++){
+        v[i].join();
+        std::cout << "i=" << b[i] << std::endl;
+    }
+
+
+    // std::thread t1(funcao_rodando_em_paralelo, 0, &b);
+
+    // std::thread t2(funcao_rodando_em_paralelo, 1, &b);
+
+    // std::thread t3(funcao_rodando_em_paralelo, 2, &b);
+
+    // std::thread t4(funcao_rodando_em_paralelo, 3, &b);
+
     
-    
-    std::cout << "Antes do join b=" << b << std::endl;
     
     // Espera até que a função acabe de executar.
-    t1.join();
-    
-    std::cout << "Depois do join b=" << b << std::endl;
+    // t1.join();
+    // t2.join();
+    // t3.join();
+    // t4.join();
 }
--- 08-threads-I/pi-numeric-integration.cpp
+++ 08-threads-I/pi-numeric-integration.cpp
@@ -1,20 +1,40 @@
 #include <iostream>
 #include <chrono>
+#include <vector>
+#include <thread>
 
 static long num_steps = 100000000;
 double step;
+
+void funcao_rodando_em_paralelo(int i,int div,double step, double *sum) {
+    double x,s;
+    for (int j = div*i; j < div*(i+1); j++) {
+        x = (j + 0.5) * step;
+        s = s + 4.0 / (1.0 + x * x);
+    }    
+    *sum=s;
+}
+
+
 int main() {
-    int i;
-    double x, pi, sum = 0.0;
+    double pi = 0.0;
     step = 1.0 / (double)num_steps;
+    int n_threads = std::thread::hardware_concurrency();
+    std::vector<std::thread> v(n_threads);
+    std::vector<double> sum(n_threads);
+    int div = num_steps/n_threads;
+    double res;
 
     auto start_time = std::chrono::high_resolution_clock::now();
-    for (i = 0; i < num_steps; i++) {
-        x = (i + 0.5) * step;
-        sum = sum + 4.0 / (1.0 + x * x);
+    for (int i = 0; i < n_threads; i++) {
+        v[i]=std::thread(funcao_rodando_em_paralelo,i,div,step, &sum[i]);
+    }
+    for  (int i = 0; i < n_threads; i++) {
+        v[i].join();
+        res+=sum[i];
     }
 
-    pi = step * sum;
+    pi = step * res;
     auto end_time = std::chrono::high_resolution_clock::now();
     auto runtime = std::chrono::duration_cast<std::chrono::seconds> (end_time - start_time);
     
