80dc50632f27673fc4b7638ee8720600bab4ff6c
EC2 Default User <ec2-user@ip-172-31-42-14.ec2.internal>
2019-11-12 13:23:58 +0000

    entradas maiores

---
+++ projeto-03/build/CMakeFiles/2-opt-sol.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/2-opt-sol.dir/2-opt-sol.cu.o"
+  "CMakeFiles/2-opt-sol.dir/cmake_device_link.o"
+  "2-opt-sol.pdb"
+  "2-opt-sol"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CUDA)
+  include(CMakeFiles/2-opt-sol.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ projeto-03/build/CMakeFiles/tsp-seq.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ec2-user/tirta/supercomp2/projeto-03/tsp-seq.cpp" "/home/ec2-user/tirta/supercomp2/projeto-03/build/CMakeFiles/tsp-seq.dir/tsp-seq.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
